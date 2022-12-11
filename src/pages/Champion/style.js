02d37e38edba9f022cfc40da1aa024956e7c8dac
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2022-12-11 15:05:48 -0300

    at

--- src/pages/Champion/style.js
+++ src/pages/Champion/style.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const StyledChampion = styled.div`
+    width: 100%;
+    max-width: 900px;
+    margin: 0 auto;
+    min-height: 100vh;
+
     flex: 1 0 auto;
     margin-top: 100px;
 
