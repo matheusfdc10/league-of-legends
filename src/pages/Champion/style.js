09d8f178fb59333638ea71190bd01f8f653b8bfb
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2022-12-10 21:37:31 -0300

    at

--- src/pages/Champion/style.js
+++ src/pages/Champion/style.js
@@ -6,11 +6,18 @@ export const StyledChampion = styled.div`
     margin: 0 auto;
     min-height: 100vh;
 
+    > hr {
+        margin: 0;
+        margin-top: 8px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
     .information {
         display: flex;
 
         .type {
-            margin: 10px;
+            margin: 20px 10px;
 
             h4 {
                 margin: 0;
@@ -25,7 +32,7 @@ export const StyledChampion = styled.div`
 
         .blurb {
             flex: 1;
-            margin: 10px;
+            margin: 20px;
 
             h4 {
                 margin: 0;
